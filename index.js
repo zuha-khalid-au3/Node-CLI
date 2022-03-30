94779033ed878bbb046b52cebe7e8a800f51e924
unknown <zuhakhalid@gmail.com>
2022-03-30 16:29:54 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const shell = require("shelljs");
+
+const init = () =>{
+    
+}
+
+const run = async () =>{
+
+};
+run();
